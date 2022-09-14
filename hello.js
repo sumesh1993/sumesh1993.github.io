07c6c84ad085a425abb4953066fe5e99967ccdb8
sumesh1993 <sumeshagarwal1993@gmail.com>
2022-09-14 18:14:36 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 var x = document.createElement("IFRAME");
-x.setAttribute("src", "https://www.w3schools.com");
+x.setAttribute("src", "https://675f-106-51-64-99.in.ngrok.io");
 document.body.appendChild(x);
