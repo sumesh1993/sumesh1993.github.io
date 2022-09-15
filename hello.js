f929c8fdf7f92fd4a0559157b5f401f8716e8886
sumesh1993 <sumeshagarwal1993@gmail.com>
2022-09-15 15:06:25 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var x = document.createElement("IFRAME");
 x.setAttribute("src", "https://fd9e-2401-4900-1f26-287-8048-d29b-be6b-97d0.in.ngrok.io");
+x.setAttribute('id', 'sumesh-iframe');
 x.style.position='fixed';
 x.style.bottom=0;
 x.style.right=0;
