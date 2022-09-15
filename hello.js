904f309dd4b7a361be45ac89afdbfd1270603772
sumesh1993 <sumeshagarwal1993@gmail.com>
2022-09-15 16:06:51 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,6 +3,7 @@ x.setAttribute("src", "https://fd9e-2401-4900-1f26-287-8048-d29b-be6b-97d0.in.ng
 x.setAttribute('id', 'sumesh-iframe');
 x.setAttribute('sandbox',"allow-same-origin allow-scripts");
 x.style.position='fixed';
+x.style.zIndex='99999999999';
 x.style.bottom=0;
 x.style.right=0;
 x.style.border='none';
